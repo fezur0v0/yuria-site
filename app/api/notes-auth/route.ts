0c0a62236a6de8f10d5c9fd4ccf8e865e771ccb4
neko <fezur0v0@gmail.com>
2026-05-31 14:06:31 +0800

    Update route.ts

--- app/api/notes-auth/route.ts
+++ app/api/notes-auth/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
+import bcrypt from 'bcryptjs'
 
 export async function POST(request: Request) {
   try {
@@ -16,7 +17,6 @@ export async function POST(request: Request) {
     if (!data) return NextResponse.json({ success: false })
 
     // 动态引入 bcryptjs
-    const bcrypt = await import('bcryptjs')
     const match = await bcrypt.compare(answer.trim(), data.value)
 
     return NextResponse.json({ success: match })
