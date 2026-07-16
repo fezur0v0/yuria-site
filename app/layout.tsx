462698e09967fa71f54e555e4a0dfac2052a6edc
neko <fezur0v0@gmail.com>
2026-07-16 17:47:05 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SiteSplash from "@/components/SiteSplash";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
   title: "Yuria",
   description: "那一天你走进了我的生命 谢谢你成为了我的几分之几",
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,7 +25,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <SiteSplash />
+        {children}
+      </body>
     </html>
   );
 }
