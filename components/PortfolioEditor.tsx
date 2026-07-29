1972e46a4e27f8ce8fb705e1430829159ce3a8a1
neko <fezur0v0@gmail.com>
2026-07-29 21:42:10 +0800

    Update PortfolioEditor.tsx

--- components/PortfolioEditor.tsx
+++ components/PortfolioEditor.tsx
@@ -176,7 +176,7 @@ const setImageAlign = (align: 'left' | 'center' | 'right') => {
       </div>
 
       {/* 编辑器内容容器 — h-full 自适应撑满屏幕，内部出现滚动条 */}
-<div className="h-[420px] lg:h-auto lg:flex-1 lg:min-h-0 bg-white border border-black/5 rounded-3xl shadow-sm overflow-hidden flex flex-col">
+<div className="flex-1 min-h-0 bg-white border border-black/5 rounded-3xl shadow-sm overflow-hidden flex flex-col">
         <div
           className="flex-1 p-8 overflow-y-auto
             [&::-webkit-scrollbar]:w-2
