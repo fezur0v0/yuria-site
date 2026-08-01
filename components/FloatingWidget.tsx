ff8037054749026006e9274acc2af877c3118acf
neko <fezur0v0@gmail.com>
2026-08-02 01:30:58 +0800

    Update FloatingWidget.tsx

--- components/FloatingWidget.tsx
+++ components/FloatingWidget.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState, useRef } from 'react';
 import Link from 'next/link';
-import { PiArrowCircleUp, PiGearSix, PiListBullets } from 'react-icons/pi';
+import { PiCaretUpLight, PiGearSix, PiListBullets } from 'react-icons/pi';
 
 interface TocItem {
   id: string;
