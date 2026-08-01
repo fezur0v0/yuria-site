ada2c7e3745ce417c5f317b997d07759e34a9b67
neko <fezur0v0@gmail.com>
2026-08-01 18:27:25 +0800

    Update StarDivider.tsx

--- components/StarDivider.tsx
+++ components/StarDivider.tsx
@@ -1,17 +1,21 @@
 import HorizontalRule from '@tiptap/extension-horizontal-rule';
 
 const StarDivider = HorizontalRule.extend({
-  parseHTML() {
-    return [{ tag: 'div[data-type="star-divider"]' }];
-  },
-  renderHTML() {
-    const unit = '☆ ★ --- ';
-    return [
-      'div',
-      { class: 'star-divider', 'data-type': 'star-divider' },
-      ['span', { class: 'star-divider-line' }, unit.repeat(40)],
-    ];
-  },
+parseHTML() {
+return 
+
+$${ tag: 'div\[data-type="star-divider"$$
+
+' }];
+},
+renderHTML() {
+const unit = '☆ ★ --- ★ ';
+return 
+
+$$'div', { class: 'star-divider', 'data-type': 'star-divider' }, \['span', { class: 'star-divider-line' }, unit.repeat(35)$$
+
+, ];
+},
 });
 
 export default StarDivider;
